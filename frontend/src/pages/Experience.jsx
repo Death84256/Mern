bd4a58984ff011ed96271ab1f29c615be3137e4e
ASI\esebial <esebial@csyoutsourcing..com>
2024-10-16 17:08:05 +0800

    new update

--- frontend/src/pages/Experience.jsx
+++ frontend/src/pages/Experience.jsx
@@ -3,8 +3,8 @@ import { TypeAnimation } from "react-type-animation"
 
 const Experience = () => {
   return (
-    <div className='border-2 border-sky-500 w-lvw h-lvh absolute top-0 right-0 p-2'>
-        <div className='border-2 p-2 mt-[85px]'>
+    <div className='w-lvw h-lvh absolute top-0 right-0 p-2'>
+        <div className='p-2 mt-[85px]'>
         <TypeAnimation
               sequence={["Project Experience", 1000, "", 1000]}
               wrapper="p"
